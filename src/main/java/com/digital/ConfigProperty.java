302e30b992d3f9a7b3319c14b13d60a71c7972c2
adnan-mahboob <adnan.mahboob@outlook.com>
2017-02-02 15:33:45 +0000

    initial commit

---
+++ src/main/java/com/digital/ConfigProperty.java
@@ -0,0 +1,22 @@
+package com.digital;
+
+public enum ConfigProperty {
+    TIMEOUT_DOM("domTimeout", "30"),
+    TIMEOUT_PAGE("pageTimeout", "30");
+
+    private String propertyName;
+    private String defaultValue;
+
+    private ConfigProperty(String propertyName, String defaultValue){
+        this.propertyName = propertyName;
+        this.defaultValue = defaultValue;
+    }
+
+    public String getValue(){
+        return System.getProperty(propertyName, defaultValue);
+    }
+
+    public int getValueAsInt(){
+        return Integer.parseInt(getValue());
+    }
+}
